3e81340c67fc9dc0d2663fd5642283949db4fbc4
freedom.yi <851989962@qq.com>
2019-06-22 02:04:09 +0800

    (测试下)完整react + webpack + node 服务器渲染的项目

--- client/component/home/home.jsx
+++ client/component/home/home.jsx
@@ -8,10 +8,10 @@ const Home = () => (
     <div className={classes.root}>
       <h2>1234</h2>
       <h2>我是首页标题</h2>
-      <div>哈哈哈哈11</div>
-      <p>this is changsha</p>
+      <div>哈哈哈哈</div>
+      <p>11---this is changsha---22</p>
       <div>服务器开发做好了呀</div>
-      <div>暂时貌似还可以样的哈11</div>
+      <div>暂时貌似还可以样的哈</div>
       <span>不要按F10就不要替换</span>
     </div>
     <div>我敢你妈妈</div>
